cec0b434597f111dc3b7a78c6fd21fd0e1a68a5f
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2024-05-30 23:05:28 -0400

    Initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,9 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'The Geaux Code Markup Guide',
+  tagline: 'The Best Damn Resource for HTML, Markdown, and Other Website Markup',
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
